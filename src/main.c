5c7aa68c5c5215d35ad6588c76aa1404b62c9476
pesrisaw <peerapol1611@gmail.com>
2025-05-17 09:34:17 +0700

    update src

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include "philo.h"
 #include "test.h"
 
+// format of the test
+// [number of philosophers] [time to die] [time to eat] [time to sleep] [number of times each philosopher must eat]]
+
 int	main(int argc, char **argv)
 {
+	
 	return (0);
 }
 
